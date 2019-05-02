86b8997ba2483aed2f3b64beaffbbc9b3ba22be4
kazuki watanabe <watkazuki03@gmail.com>
2019-05-02 11:35:01 +0900

    Add response object

--- hello.py
+++ hello.py
@@ -1,12 +1,12 @@
 from src.app import App
+from src.response import Response
 from wsgiref.simple_server import make_server
 
 app = App()
 
 @app.route('^/$', 'GET')
-def hello(request, start_response):
-    start_response('200 OK', [('Content-type', 'text/plain; charset=utf-8')])
-    return [b'Hello d World']
+def hello(request):
+    return Response('Hello world')
 
 if __name__ == '__main__':
     httpd = make_server('', 8000, app)
--- src/app.py
+++ src/app.py
@@ -1,4 +1,5 @@
 from .router import Router
+from .request import Request
 
 class App:
     def __init__(self):
@@ -11,7 +12,9 @@ class App:
         return decorator(callback) if callback else decorator
 
     def __call__(self, env, start_response):
-        method = env['REQUEST_METHOD'].upper()
-        path = env['PATH_INFO'] or '/'
-        callback, kwargs = self.router.match(method, path)
-        return callback(env, start_response, **kwargs)
+        request = Request(env)
+        callback, url_vars = self.router.match(request.method, request.path)
+
+        response = callback(request, **url_vars)
+        start_response(response.status_code, response.header_list)
+        return response.body
